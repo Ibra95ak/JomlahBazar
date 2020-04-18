72e3f8173a993f7eb5ccac6061df128b628c7762
nabeel0489 <s-nabeel@live.com>
2020-04-18 16:27:55 +0400

    Updated UI using base template

--- AdminPanel/footer.php
+++ AdminPanel/footer.php
@@ -1,86 +1,87 @@
 </div>
 <!-- begin:: Footer -->
-					<div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
-						<div class="kt-container  kt-container--fluid ">
-							<div class="kt-footer__copyright">
-								2020&nbsp;&copy;&nbsp;<a href="http://keenthemes.com/metronic" target="_blank" class="kt-link">Keenthemes</a>
-							</div>
-							<div class="kt-footer__menu">
-								<a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">About</a>
-								<a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Team</a>
-								<a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Contact</a>
-							</div>
-						</div>
-					</div>
+<div class="kt-footer  kt-grid__item kt-grid kt-grid--desktop kt-grid--ver-desktop" id="kt_footer">
+    <div class="kt-container  kt-container--fluid ">
+        <div class="kt-footer__copyright">
+            2020&nbsp;&copy;&nbsp;<a href="http://keenthemes.com/metronic" target="_blank"
+                class="kt-link">Keenthemes</a>
+        </div>
+        <div class="kt-footer__menu">
+            <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">About</a>
+            <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Team</a>
+            <a href="http://keenthemes.com/metronic" target="_blank" class="kt-footer__menu-link kt-link">Contact</a>
+        </div>
+    </div>
+</div>
 
-					<!-- end:: Footer -->
-				</div>
-			</div>
-		</div>
+<!-- end:: Footer -->
+</div>
+</div>
+</div>
 
-		<!-- end:: Page -->
-
-
-		<!-- begin::Scrolltop -->
-		<div id="kt_scrolltop" class="kt-scrolltop">
-			<i class="fa fa-arrow-up"></i>
-		</div>
-
-		<!-- end::Scrolltop -->
+<!-- end:: Page -->
 
 
-		<!-- begin::Global Config(global config for global JS sciprts) -->
-		<script>
-			var KTAppOptions = {
-				"colors": {
-					"state": {
-						"brand": "#5d78ff",
-						"dark": "#282a3c",
-						"light": "#ffffff",
-						"primary": "#5867dd",
-						"success": "#34bfa3",
-						"info": "#36a3f7",
-						"warning": "#ffb822",
-						"danger": "#fd3995"
-					},
-					"base": {
-						"label": [
-							"#c5cbe3",
-							"#a1a8c3",
-							"#3d4465",
-							"#3e4466"
-						],
-						"shape": [
-							"#f0f3ff",
-							"#d9dffa",
-							"#afb4d4",
-							"#646c9a"
-						]
-					}
-				}
-			};
-		</script>
+<!-- begin::Scrolltop -->
+<div id="kt_scrolltop" class="kt-scrolltop">
+    <i class="fa fa-arrow-up"></i>
+</div>
 
-		<!-- end::Global Config -->
+<!-- end::Scrolltop -->
 
-		<!--begin::Global Theme Bundle(used by all pages) -->
-		<script src="assets/plugins/global/plugins.bundle.js" type="text/javascript"></script>
-		<script src="assets/js/scripts.bundle.js" type="text/javascript"></script>
 
-		<!--end::Global Theme Bundle -->
+<!-- begin::Global Config(global config for global JS sciprts) -->
+<script>
+var KTAppOptions = {
+    "colors": {
+        "state": {
+            "brand": "#5d78ff",
+            "dark": "#282a3c",
+            "light": "#ffffff",
+            "primary": "#5867dd",
+            "success": "#34bfa3",
+            "info": "#36a3f7",
+            "warning": "#ffb822",
+            "danger": "#fd3995"
+        },
+        "base": {
+            "label": [
+                "#c5cbe3",
+                "#a1a8c3",
+                "#3d4465",
+                "#3e4466"
+            ],
+            "shape": [
+                "#f0f3ff",
+                "#d9dffa",
+                "#afb4d4",
+                "#646c9a"
+            ]
+        }
+    }
+};
+</script>
 
-		<!--begin::Page Vendors(used by this page) -->
-		<script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
-		<script src="//maps.google.com/maps/api/js?key=AIzaSyBTGnKT7dt597vo9QgeQ7BFhvSRP4eiMSM" type="text/javascript"></script>
-		<script src="assets/plugins/custom/gmaps/gmaps.js" type="text/javascript"></script>
+<!-- end::Global Config -->
 
-		<!--end::Page Vendors -->
+<!--begin::Global Theme Bundle(used by all pages) -->
+<script src="assets/plugins/global/plugins.bundle.js" type="text/javascript"></script>
+<script src="assets/js/scripts.bundle.js" type="text/javascript"></script>
+<!--end::Global Theme Bundle -->
 
-		<!--begin::Page Scripts(used by this page) -->
-		<script src="assets/js/pages/dashboard.js" type="text/javascript"></script>
-		<script src="assets/js/pages/crud/metronic-datatable/advanced/row-details.js" type="text/javascript"></script>
-		<!--end::Page Scripts -->
-	</body>
+<!--begin::Page Vendors(used by this page) -->
+<script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js" type="text/javascript"></script>
+<script src="//maps.google.com/maps/api/js?key=AIzaSyBTGnKT7dt597vo9QgeQ7BFhvSRP4eiMSM" type="text/javascript"></script>
+<script src="assets/plugins/custom/gmaps/gmaps.js" type="text/javascript"></script>
 
-	<!-- end::Body -->
+<!--end::Page Vendors -->
+
+<!--begin::Page Scripts(used by this page) -->
+<script src="assets/js/pages/dashboard.js" type="text/javascript"></script>
+<script src="assets/js/pages/crud/metronic-datatable/advanced/row-details.js" type="text/javascript"></script>
+<!--end::Page Scripts -->
+</body>
+
+<!-- end::Body -->
+
 </html>
