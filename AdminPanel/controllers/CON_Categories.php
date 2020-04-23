de4708f83b57dffae9c1df8fe9a252f631317b64
Ibra-ak <48854444+Ibra-ak@users.noreply.github.com>
2020-04-23 11:15:37 +0400

    Categories decleration
    Developed the class,controller and the front-end page to show the data plus deployed the connection with the front end page with backend code

---
+++ AdminPanel/controllers/CON_Categories.php
@@ -0,0 +1,18 @@
+<?php
+//Get base class
+require_once '../libraries/Base.php';
+//Get Categories class
+require_once '../libraries/Ser_Categories.php';
+$db = new Ser_Categories();
+//results array
+$results=array();
+//get all leads details
+$getAll_categories = $db->GetCategories();
+foreach($getAll_categories as $category){
+    array_push($results,$category);
+}
+
+$fp = fopen('json/categories.json', 'w');
+fwrite($fp, json_encode($results));
+fclose($fp);
+?>
---
+++ AdminPanel/libraries/Ser_Categories.php
@@ -0,0 +1,49 @@
+<?php 
+class Ser_Categories {
+    private $conn;
+    // constructor
+    function __construct() {
+        require_once 'DB_Connect.php';
+        // connecting to database
+        $db = new Db_Connect();
+        $this->conn = $db->connect();
+    }
+    // destructor
+    function __destruct() {
+        
+    }
+    	
+    /**
+     * Get all categories 
+     * returns json/Null
+     */
+    public function GetCategories() {
+        $stmt = $this->conn->prepare("CALL sp_GetCategories()");
+        if ($stmt->execute()) {
+            $categories = $stmt->get_result()->fetch_all(MYSQLI_ASSOC); //fetch categories data and store in array
+            $stmt->close();
+            if ($categories==true) {
+                return $categories;
+            }
+        } else return NULL;
+    }
+    
+    /**
+     * Get all categories 
+     * params category Id
+     * returns json/Null
+     */
+    public function GetCategoryById($categoryId) {
+        $stmt = $this->conn->prepare("CALL sp_GetCategoryById(?)");
+        $stmt->bind_param("i",$categoryId);
+        if ($stmt->execute()) {
+            $categories = $stmt->get_result()->fetch_assoc(); //fetch categories data and store in array
+            $stmt->close();
+            if ($categories==true) {
+                return $categories;
+            }
+        } else return NULL;
+    }
+
+}
+?>
