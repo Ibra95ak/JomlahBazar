4f0e838d4a07870db7b0c06523fa89188b418f62
aliawaismalik0007 <ali.awais5044@gmail.com>
2020-05-23 01:30:17 +0400

    Supplier card updated and fixed product card

---
+++ UserInterface/assets/js/custom.js
@@ -0,0 +1,16 @@
+var $star_rating = $('.star-rating .fa');
+
+var SetRatingStar = function() {
+  return $star_rating.each(function() {
+    if (parseInt($star_rating.siblings('input.rating-value').val()) >= parseInt($(this).data('rating'))) {
+      return $(this).removeClass('fa-star-o').addClass('fa-star');
+    } else {
+      return $(this).removeClass('fa-star').addClass('fa-star-o');
+    }
+  });
+};
+
+SetRatingStar();
+$(document).ready(function() {
+
+});
