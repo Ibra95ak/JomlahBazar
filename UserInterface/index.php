66048a348a1049397b2b58e014979293d13cd05d
aliawaismalik0007 <ali.awais5044@gmail.com>
2020-05-31 11:02:39 +0400

    login register pull

--- UserInterface/index.php
+++ UserInterface/index.php
@@ -27,245 +27,158 @@
 </head>
 
 <body>
-  <!-- Navigation with search bar-->
-  <nav class="navbar navbar-expand-lg navbar-dark sticky-top navbar-search-bar">
-    <div class="container-fluid">
-      <div class="row" style="flex: auto; height: 100%; align-items: center;">
-        <div class="col-lg-2">
-          <a class="navbar-brand" href="index.html">
-            <h5>JomlaBazar</h5>
-          </a>
-        </div>
+     <!-- Navigation with search bar-->
+     <nav
+      class="navbar navbar-expand-lg navbar-dark sticky-top navbar-search-bar"
+    >
+      <div class="container-fluid">
+        <div class="row" style="flex: auto; height: 100%; align-items: center;">
+          <div class="col-lg-2">
+            <a class="navbar-brand" href="index.html">
+              <h5>JomlaBazar</h5>
+            </a>
+          </div>
 
-        <div class="col-lg-6">
-          <div class="row pt-lg-0 pt-2">
-            <div class="top-dropdown">
-              <div class="all-cate custom-select2">
-                <select>
-                  <option>All</option>
-                  <option>Brand</option>
-                  <option>Products</option>
-                  <option>Supplier</option>
-                  <option>Buyer</option>
-                  <option>Location</option>
-                </select>
+          <div class="col-lg-6">
+            <div class="row pt-lg-0 pt-2">
+              <div class="top-dropdown">
+                <div class="all-cate custom-select2">
+                  <select>
+                    <option>All</option>
+                    <option>Brand</option>
+                    <option>Products</option>
+                    <option>Supplier</option>
+                    <option>Buyer</option>
+                    <option>Location</option>
+                  </select>
+                </div>
               </div>
-            </div>
 
-            <div class="top-dropdown">
-              <div class="all-cate custom-select2 sub-menu">
-                <select disabled>
-                  <option>Sub All</option>
-                  <option>Brand</option>
-                  <option>Products</option>
-                  <option>Supplier</option>
-                  <option>Buyer</option>
-                  <option>Location</option>
-                </select>
+              <div class="top-dropdown">
+                <div class="all-cate custom-select2 sub-menu">
+                  <select disabled>
+                    <option>Sub All</option>
+                    <option>Brand</option>
+                    <option>Products</option>
+                    <option>Supplier</option>
+                    <option>Buyer</option>
+                    <option>Location</option>
+                  </select>
+                </div>
               </div>
-            </div>
 
-            <div class="col p-lg-0 pt-lg-0 pt-2">
-              <div class="input-group filter-by">
-                <input type="hidden" name="search_param" value="all" id="search_param" />
-                <input type="text" class="form-control" name="x" placeholder="What do you need?" />
-                <span class="input-group-btn">
-                  <button class="btn btn-default search-bt" type="button">
-                    <i class="fa fa-search"></i>
-                  </button>
-                </span>
+              <div class="col p-lg-0 pt-lg-0 pt-2">
+                <div class="input-group filter-by">
+                  <input
+                    type="hidden"
+                    name="search_param"
+                    value="all"
+                    id="search_param"
+                  />
+                  <input
+                    type="text"
+                    class="form-control"
+                    name="x"
+                    placeholder="What do you need?"
+                  />
+                  <span class="input-group-btn">
+                    <button class="btn btn-default search-bt" type="button">
+                      <i class="fa fa-search"></i>
+                    </button>
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div class="col-lg-4">
-          <div class="row no-gutters flex-nowrap justify-content-around">
-            <a class="nav-a nav-a-2 nav-tool">
-              <span class="nav-line-1">Hello sigin</span>
-              <span class="nav-line-2 text-nowrap">Account & lists
-                <i class="fa fa-caret-down nav-icon nav-arrow"></i></span>
-            </a>
+          <div class="col-lg-4">
+            <div class="row flex-nowrap justify-content-around">
+              <a
+                href="#"
+                class="language-toggle nav-a nav-a-2 nav-tool dropdown-toggle d-flex justify-content-center"
+                style="color: white; padding: 5px;"
+              >
+              <span class="nav-line-2 text-nowrap"><img src="./assets/images/us-flag.jpg" width="24px" height="13px"/>
+              <i class="fa fa-caret-down"></i>
+              </span>
+              <div class="language-dropdown dropdown dropdown-menu animate slideIn">
+                <a
+                  class="dropdown-item dropdown-text "
+                  href="#"
+                  >English - EN</a
+                >
+                <a class="dropdown-item dropdown-text" href="#">Espanol - ES</a>
+                <a class="dropdown-item dropdown-text" href="#">Deutsch - DE</a>
+                <a class="dropdown-item dropdown-text" href="#"
+                  >العربية - AR</a
+                >
+              </div>
+              </a>
+              <a
+                class="login-register-toggle nav-a nav-a-2 nav-tool dropdown-toggle"
+              >
+                <span class="nav-line-1">Hello sigin</span>
+                <span class="nav-line-2 text-nowrap"
+                  >Account & lists
+                  <i class="fa fa-caret-down nav-icon nav-arrow"></i
+                ></span>
+                <div class="dropdown login-register-dropdown dropdown-menu animate slideIn">
+                  <a
+                    class="dropdown-item dropdown-text"
+                    href="/JomlahBazar/UserInterface/login.php"
+                    >Login</a
+                  >
+                  <a class="dropdown-item dropdown-text" href="my-account.html">My Account</a>
+                  <a class="dropdown-item dropdown-text" href="register.html">Register</a>
+                  <a class="dropdown-item dropdown-text" href="forgot-password.html"
+                    >Forgot Password</a
+                  >
+                </div>
+              </a>
 
-            <a class="nav-a nav-a-2 nav-tool">
-              <span class="nav-line-1">Returns</span>
-              <span class="nav-line-2 text-nowrap">& Orders </span>
-            </a>
+              <a class="nav-a nav-a-2 nav-tool">
+                <span class="nav-line-1">Returns</span>
+                <span class="nav-line-2 text-nowrap">& Orders </span>
+              </a>
 
-            <a class="nav-a nav-a-2 text-center nav-tool">
-              <div style="display: flex;">
-                <div class="nav-line-2 text-nowrap" style="width: 60%; position: relative; float: left;">
-                  <span class="nav-line-1 nav-cart-count font-weight-bold" style="
+              <a class="nav-a nav-a-2 text-center nav-tool">
+                <div style="display: flex;">
+                  <div
+                    class="nav-line-2 text-nowrap"
+                    style="width: 60%; position: relative; float: left;"
+                  >
+                    <span
+                      class="nav-line-1 nav-cart-count font-weight-bold"
+                      style="
                         position: absolute;
                         top: 0;
                         left: 43%;
                         color: #f08804;
                         font-size: 16px;
                         width: 19px;
-                      ">9</span>
-                  <img src="./assets/images/cart.png" />
-                </div>
+                      "
+                      >9</span
+                    >
+                    <img src="./assets/images/cart.png" />
+                  </div>
 
-                <div style="
+                  <div
+                    style="
                       width: 40%;
                       right: 8px;
                       position: relative;
                       top: 22px;
-                    ">
-                  Cart
-                </div>
-              </div>
-            </a>
-          </div>
-          <!-- <div class="" id="navbarResponsive">
-        <div class="rate-price nav-1">
-          <ul>
-            <li class="dropdown">
-              <a class="dropdown-toggle" href="" data-toggle="dropdown">
-                <i class="fa fa-user-circle-o" aria-hidden="true"></i
-              ></a>
-              <div
-                class="dropdown-menu dropdown-menu-right animate slideIn"
-              >
-                <a class="dropdown-item" href="login.html">Login</a>
-                <a class="dropdown-item" href="my-account.html"
-                  >My Account</a
-                >
-                <a class="dropdown-item" href="register.html">Register</a>
-                <a class="dropdown-item" href="forgot-password.html"
-                  >Forgot Password</a
-                >
-              </div>
-            </li>
-            <li>
-              <a href="wishlist.html"
-                ><i class="fa fa-heart-o" aria-hidden="true"></i
-                ><span class="circle-2">1</span></a
-              >
-            </li>
-            <li class="dropdown">
-              <a
-                class="dropdown-toggle link"
-                href=""
-                data-toggle="dropdown"
-                ><i class="fa fa-shopping-bag" aria-hidden="true"></i
-                ><span class="circle-2">1</span></a
-              >
-              <div
-                class="dropdown-menu dropdown-menu2 dropdown-menu-right animate slideIn"
-              >
-                <div class="container">
-                  <div class="row">
-                    <div class="col-md-3">
-                      <img
-                        src="./assets/images/fruits/img-1.jpg"
-                        alt=""
-                        title=""
-                        class="img-fluid"
-                      />
-                    </div>
-                    <div class="col-md-9">
-                      <p>
-                        1 x Product Name...
-                        <span class="price">$ 14.70</span>
-                      </p>
-                      <a href="" class="close">x</a>
-                    </div>
-                    <div class="col-md-12">
-                      <hr />
-                    </div>
-                    <div class="col-md-3">
-                      <img
-                        src="./assets/images/fruits/img-2.jpg"
-                        alt=""
-                        title=""
-                        class="img-fluid"
-                      />
-                    </div>
-                    <div class="col-md-9">
-                      <p>
-                        1 x Product Name...
-                        <span class="price">$ 14.70</span>
-                      </p>
-                      <a href="" class="close">x</a>
-                    </div>
-                    <div class="col-md-12">
-                      <hr />
-                    </div>
-                    <div class="col-md-3">
-                      <img
-                        src="assets/images/fruits/img-3.jpg"
-                        alt=""
-                        title=""
-                        class="img-fluid"
-                      />
-                    </div>
-                    <div class="col-md-9">
-                      <p>
-                        1 x Product Name...
-                        <span class="price">$ 14.70</span>
-                      </p>
-                      <a href="" class="close">x</a>
-                    </div>
-                    <div class="col-md-12">
-                      <hr />
-                    </div>
-                    <div class="col-md-3">
-                      <img
-                        src="assets/images/fruits/img-4.jpg"
-                        alt=""
-                        title=""
-                        class="img-fluid"
-                      />
-                    </div>
-                    <div class="col-md-9">
-                      <p>
-                        1 x Product Name...
-                        <span class="price">$ 14.70</span>
-                      </p>
-                      <a href="" class="close">x</a>
-                    </div>
-                    <div class="col-md-12">
-                      <hr />
-                    </div>
-                    <div class="col-md-3">
-                      <p class="font-15">Tax</p>
-                    </div>
-                    <div class="col-md-9 text-right">
-                      <span class="font-15">$ 2.80</span>
-                    </div>
-                    <div class="col-md-12">
-                      <hr />
-                    </div>
-                    <div class="col-md-3">
-                      <p class="font-15"><strong>Total</strong></p>
-                    </div>
-                    <div class="col-md-9 text-right">
-                      <span class="font-15"
-                        ><strong>$ 10.80</strong></span
-                      >
-                    </div>
-                    <div class="col-md-12">
-                      <hr />
-                    </div>
-                    <div class="col-md-12 text-center">
-                      <input
-                        type="button"
-                        value="Check out"
-                        class="btn check-out w-100"
-                      />
-                    </div>
+                    "
+                  >
+                    Cart
                   </div>
                 </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div> -->
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
 
 
@@ -2225,6 +2138,11 @@
     </div>
   </footer>
 
+
+   <!-- Custom Javascrip file -->
+   <script src="assets/js/custom.js"></script>
+     <!-- Custom Javascrip file -->
+     
   <script src="assets/vendor/home-banner/jquery.min.js"></script>
   <script src="assets/vendor/jquery/jquery.min.js"></script>
   <script src="assets/js/ajax.js"></script>
@@ -2254,13 +2172,6 @@
   <script src="assets/vendor/slick/slick.min.js"></script>
   <script src="assets/vendor/slick/script.js"></script>
   <!--banner js-->
-  <!-- <script src="assets/vendor/revolution/vendor/revslider/js/jquery.themepunch.tools.min.js"></script>
-    <script src="assets/vendor/revolution/vendor/revslider/js/jquery.themepunch.revolution.min.js"></script>
-    <script src="assets/vendor/revolution/vendor/revslider/js/extensions/revolution.extension.actions.min.js"></script>
-    <script src="assets/vendor/revolution/vendor/revslider/js/extensions/revolution.extension.layeranimation.min.js"></script>
-    <script src="assets/vendor/revolution/vendor/revslider/js/extensions/revolution.extension.navigation.min.js"></script>
-    <script src="assets/vendor/revolution/vendor/revslider/js/extensions/revolution.extension.parallax.min.js"></script>
-    <script src="assets/vendor/revolution/vendor/revslider/js/extensions/revolution.extension.slideanims.min.js"></script> -->
   <script src="assets/js/banner.js"></script>
   <!--banner js-->
   <!--scrolltop-->
